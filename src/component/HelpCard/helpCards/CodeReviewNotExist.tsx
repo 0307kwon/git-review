08dbf3acd3b5ed8c0097a46c6f2a6153d00d5813
권세진 <0307kwon@gmail.com>
2021-08-22 14:11:50 +0900

    fix: UI에 보여지는 PR 단어를 '코드리뷰'로 바꿈

--- src/component/HelpCard/helpCards/CodeReviewNotExist.tsx
+++ src/component/HelpCard/helpCards/CodeReviewNotExist.tsx
@@ -5,8 +5,8 @@ const CodeReviewNotExist = () => {
   return (
     <HelpCardTemplate>
       {{
-        title: "🚧 PR 목록이 아직 등록되지 않았어요",
-        subTitle: "프로필 - 설정을 클릭해 PR 목록을 업데이트 해보세요!",
+        title: "🚧 코드 리뷰 목록이 아직 등록되지 않았어요",
+        subTitle: "프로필 - 설정을 클릭해 리뷰 목록을 업데이트 해보세요!",
       }}
     </HelpCardTemplate>
   );
