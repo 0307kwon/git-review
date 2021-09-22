6b12de93d83190083b81c09c06b880b2e0d29411
권세진 <0307kwon@gmail.com>
2021-09-22 16:42:26 +0900

    refactor: 코드 블럭 색상 수정

--- src/constant/palette.ts
+++ src/constant/palette.ts
@@ -3,21 +3,20 @@ export enum PALETTE {
 
   WHITE = "#FFFFFF",
 
-  GRAY_100 = "#e9ecef",
+  GRAY_100 = "#f6f6f6",
   GRAY_200 = "#DDDDDD",
   GRAY_300 = "#BBBBBB",
   GRAY_400 = "#999999",
   GRAY_500 = "#777777",
 
-  PRIMARY_200 = "#CDC9FE",
+  PRIMARY_200 = "#dedbff",
   PRIMARY_400 = "#a29bfe",
   PRIMARY_500 = "#8B83F2",
   PRIMARY_600 = "#493CFF",
 
-  RED_100 = "#FFCBCB",
+  RED_100 = "#ffebe9",
   RED_200 = "#FF9D9D",
   RED_400 = "#FF3C3C",
 
-  GREEN_100 = "#BCF9DF",
-  GREEN_200 = "#78E1B5",
+  GREEN_100 = "#e6ffec",
 }
