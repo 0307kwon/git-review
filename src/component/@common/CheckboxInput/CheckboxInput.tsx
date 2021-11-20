7cb8a59aa94b3bc3ebcc513827399386a87df8fd
권세진 <0307kwon@gmail.com>
2021-11-21 01:46:35 +0900

    fix: 라디오버튼 -> 체크 박스로 변경

--- src/component/@common/CheckboxInput/CheckboxInput.tsx
+++ src/component/@common/CheckboxInput/CheckboxInput.tsx
@@ -1,17 +1,17 @@
 import React, { InputHTMLAttributes } from "react";
-import { Label } from "./RadioButtonInput.styles";
+import { Label } from "./CheckboxInput.styles";
 
 interface Props extends Omit<InputHTMLAttributes<HTMLInputElement>, "type"> {
   labelText: string;
 }
 
-const RadioButtonInput = ({ labelText, ...option }: Props) => {
+const CheckboxInput = ({ labelText, ...option }: Props) => {
   return (
     <Label>
-      <input className="radio-input" type="radio" {...option}></input>
+      <input className="radio-input" type="checkbox" {...option}></input>
       <span className="radio-mark">{labelText}</span>
     </Label>
   );
 };
 
-export default RadioButtonInput;
+export default CheckboxInput;
